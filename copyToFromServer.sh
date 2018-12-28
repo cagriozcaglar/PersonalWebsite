93a097abb9de563b7b508f009b2fdbcd48b22ff7
Cagri Ozcaglar <cagriozcaglar@gmail.com>
2018-12-28 14:29:40 -0800

    Updating website in December 2018 with links to new articles and new work updates

--- copyToFromServer.sh
+++ copyToFromServer.sh
@@ -1,3 +1,5 @@
+# Copy from server to local
 scp ozcagc2@linux.cs.rpi.edu:/cs/ozcagc2/public.html/index.html .
-scp *.html *.css *.pdf ozcagc2@linux.cs.rpi.edu:/cs/ozcagc2/public.html/.
 
+# Copy from local to server
+scp *.html *.css *.pdf ozcagc2@linux.cs.rpi.edu:/cs/ozcagc2/public.html/.
